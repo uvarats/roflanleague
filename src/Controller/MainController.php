f878b2b99442b91794a42801e2356ac6b3e82225
on3s228 <kryashow@yandex.ru>
2022-07-16 18:53:28 +0300

    init\

---
+++ src/Controller/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use PhpParser\Node\Expr\Cast\Double;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
